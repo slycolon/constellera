c3c6f071cf29a3cc950c19cff43f5e9f46355498
slycolon <jasonphillips130@gmail.com>
2018-04-23 10:51:56 -0500

    Update begin.h

--- begin.h
+++ begin.h
@@ -19,12 +19,26 @@ int begin()
 
 int newGame()
 {
+	ifstream savefile;
+	
 	
 }
 
 int loadGame()
 {
+	bool loopEnd = true;
+	int input = 0;
+	ifstream saveFile;
 	
+	do
+	{
+		cout<<"choose a file to load from (1-5)" <<endl <<endl <<">";
+		cin >> input;
+		switch (input)
+		{
+			
+		}
+	} while (loopEnd == false);
 }
 
 void help()
