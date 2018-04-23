806ff04045a2d02351e63906a8d335e27be4808c
slycolon <jasonphillips130@gmail.com>
2018-04-23 09:39:29 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,5 +8,34 @@ using namespace std;
 
 int main()
 {
-  startGame();
+  int x;
+  bool endLoop = true;
+  cout<<"welcome to constellera: the game"<<endl<<endl;
+  
+  x = begin();
+	
+  
+	do
+	{
+  	if (x == 0)
+  	{
+  	  newGame();
+  	}
+  	else if (x == 1)
+  	{
+  	  loadGame();
+  	}
+  	else if (x == 2)
+  	{
+  	  help();
+  	}
+  	else if (x == 3)
+  	{
+  	  return 0;
+  	}
+  	else
+		{
+			cout<<"invalid. please try again.";
+			endLoop = false;
+		}
 }
